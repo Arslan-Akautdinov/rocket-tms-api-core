f427724c19c6846026cd8433627bacc1ccefcffc
Arslan-Akautdinov <64992381+Arslan-Akautdinov@users.noreply.github.com>
2023-01-21 13:40:40 +0300

    add new controller (TestController)

---
+++ src/main/java/org/rocket/tms/api/core/controllers/TestController.java
@@ -0,0 +1,12 @@
+package org.rocket.tms.api.core.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping
+    public String get() {
+        return "HelloWorld";
+    }
+}
